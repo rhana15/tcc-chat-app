c811a0ff845481b0cfac48c7dfc093a4de66faea
Rhana Petreski <115051928+rhana15@users.noreply.github.com>
2026-06-28 10:34:38 -0400

    Delete Chat App directory

--- Chat App/src/main/java/br/com/rhana/tcc/chatapp/client/Launcher.java
+++
@@ -1,8 +0,0 @@
-package br.com.rhana.tcc.chatapp.client;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        javafx.application.Application.launch(ChatApplication.class, args);
-    }
-}
